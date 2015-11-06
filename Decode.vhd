dc5e60d1b5babca2963697bcb0cf29f695e65a92
dallen72 <dallen72@uncc.edu>
2015-11-05 23:09:47 -0500

    Update Decode.vhd

--- Decode.vhd
+++ Decode.vhd
@@ -5,13 +5,13 @@ use ieee.numeric_std.all;
 
 entity decoder is
   port (
+    rst : in std_logic;
     instruction : in std_logic_vector(15 downto 0);
     reg_wr_addr_sel : out std_logic;
+    mem_wr_en : out std_logic;
     mem_addr_sel : out std_logic_vector(1 downto 0);
-    opcode : out std_logic_vector(7 downto 0); -- ALU select
-    instr_lower_bits : out std_logic_vector(7 downto 0);
-    is_branch_instruction : out std_logic;  -- tells if the opcode is a branch istruction
-    reg_file_wr_sel : out std_logic
+    reg_file_Din_sel : out std_logic;
+    jmp_en : out std_logic -- goes to fetch
   );
 end decoder;
 
@@ -37,23 +37,27 @@ begin
     elsif ( (var_opcode(7 downto 4) = "1010") or (var_opcode(7 downto 4) = "1011") ) then -- LD Register, STR Register
       mem_addr_sel <= "10"; -- set to lower 8 bits of instruction
     else
-      mem_addr_sel <= "00";
+       mem_addr_sel <= "00";
     end if;
 
     if (var_opcode = "01011000") then -- MOV Instruction
-      reg_file_wr_sel <= '1'; -- set to write to R[y]
+      reg_file_Din_sel <= '1'; -- set to write to R[y]
     else
-      reg_file_wr_sel <= '0'; -- set to write to R[x]
+      reg_file_Din_sel <= '0'; -- set to write to R[x]
     end if;
     
-    if (var_opcode(7 downto 6) = "11") then
-      is_branch_instruction <= '1';
+    if ( (var_opcode(7 downto 4) = "1001") or (var_opcode(7 downto 4) = "1011") ) then -- store indirect and store register
+      mem_wr_en <= '1';
     else
-      is_branch_instruction <= '0';
+      mem_wr_en <= '0';
+    end if;
+    
+    if (var_opcode(7 downto 4) = "1100") then
+      jmp_en <= '1';
+    else
+      jmp_en <= '0';
     end if;
 
-    opcode <= var_opcode;
-    instr_lower_bits <= instruction(7 downto 0);
 
   end process;
   
